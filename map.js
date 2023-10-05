497fe5a74787fcbf2a70d0d086a419e4e1fe76fc
huichu <b07204048@gmail.com>
2023-10-05 21:46:23 +0800

    add study area

--- map.js
+++ map.js
@@ -17,11 +17,13 @@ async function initializeMap() {
 
 	var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
   
-	var random_point = await loadGeoJSON(); // 等待异步操作完成
+	var random_point = await loadRandomPoint(); // 等待异步操作完成
 	layerControl.addOverlay(random_point, "Random points");
-  }
+	var study_area = await loadStudyArea(); // 等待异步操作完成
+	layerControl.addOverlay(study_area, "Study area");
+}
   
-  async function loadGeoJSON() {
+async function loadRandomPoint() {
 	try {
 	  const response = await fetch('./data/random_point.geojson');
 	  if (!response.ok) {
@@ -37,7 +39,23 @@ async function initializeMap() {
 	  console.error(error);
 	}
 }
-  
+
+async function loadStudyArea() {
+	try {
+	  const response = await fetch('./data/study_area.geojson');
+	  if (!response.ok) {
+		throw new Error('Failed to load GeoJSON data');
+	  }
+	  const data = await response.json();
+	  
+	  // 处理加载的 GeoJSON 数据
+	  var getpoints = L.geoJSON(data);
+	  console.log(getpoints);
+	  return getpoints;
+	} catch (error) {
+	  console.error(error);
+	}
+}
   // 调用初始化函数
 initializeMap();
   
